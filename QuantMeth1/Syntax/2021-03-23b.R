afe74c52676c7cf9a062be65c01b83c287cd9ec4
Sebastian Sauer <ssauer@posteo.de>
2021-03-23 17:33:32 +0100

    update

--- QuantMeth1/Syntax/2021-03-23b.R
+++ QuantMeth1/Syntax/2021-03-23b.R
@@ -4,3 +4,40 @@
 4+1
 5+2  # Strg-Enter
 
+
+# Hallo R, was machst du heute Abend?
+
+
+
+1 / 200 * 30
+#> [1] 0.15
+(59 + 73 + 2) / 3
+#> [1] 44.66667
+sin(pi / 2)
+#> [1] 1
+
+
+
+alter <- 3.14  # besser  DEZIMALPUNKT!!!
+alter = 3.14  # auch ok
+
+
+# Funktion/Befehl:
+
+#name(argument)
+#
+seq(1, 10^4)
+
+gruss <- "hallo"
+gruss <- "hallo"
+
+
+a = 2.71
+b <- 3.14
+
+a == 3.14  # logische Prüfung
+a > 3
+
+a == "hallo"
+
+seq(3, 7)
